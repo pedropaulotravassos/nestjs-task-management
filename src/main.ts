d5e9152f7b57b826eede2fd8ef63387c810c3d18
pedropaulotravassos <pedropaulotravassos@gmail.com>
2024-07-11 17:32:38 -0300

    handle cors

--- src/main.ts
+++ src/main.ts
@@ -9,6 +9,11 @@ async function bootstrap() {
   const logger = new Logger('bootstrap');
   const app = await NestFactory.create(AppModule);
 
+  // habilita cors somente em dev
+  if (process.env.NODE_ENV === 'development') {
+    app.enableCors();
+  }
+
   const port = process.env.PORT || serverConfig.port;
   await app.listen(port);
   logger.log(`App listening on port ${port}`);
